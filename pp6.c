bce5a1e73fcfaa94f799a56c63903315a8fe6801
Ivan-Djientcheu <ivan.djientcheu-tchamedeu.stud.thga.de>
2022-05-19 21:25:14 +0200

    Hello World with ASCII Art

--- pp6.c
+++ pp6.c
@@ -2,5 +2,9 @@
 int main()
 {	
 	printf("Hello Word");
+	printf("#  #  #  #  #       #     #            #   #\n");
+        printf("#  # # # #  #  ##   #     #  ##   # #  #  ##\n");
+	printf("#### ##  #  # #  #  #  #  # #  #  #    # # #\n");
+	printf("#  #  ## #  #  ##     #  #   ##	  #    #  ##\n");
 	return 0;
 }
