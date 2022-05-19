95bd196b23e800672dcdbf8661a0701953e19253
ThoBerg <103457454+ThoBerg@users.noreply.github.com>
2022-05-19 21:45:26 +0200

    Update format.c

--- format.c
+++ format.c
@@ -5,7 +5,7 @@ int main() {
     float Gleitkommazahl = 4.5;
     int Ganzzahl = 1;
     char Character = "A";
-    char String = "abcdef";
+    char String[] = { "Hallo Welt\n" };
   
   
 	printf("Gleitkommazahl");
